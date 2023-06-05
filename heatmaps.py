d249703440d036b27215e30c71f789434c45f3d3
Olga Scherbakova <106417400+yatomiracle@users.noreply.github.com>
2023-06-05 14:45:07 +0700

    heatmaps.py

--- heatmaps.py
+++ heatmaps.py
@@ -1,12 +1,11 @@
-# -*- coding: utf-8 -*-
-
+""" Данный код принимает на вход матрицу в формате xlsx и строит по ней тепловую диаграмму """
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-df = pd.read_excel("file.xlsx", sheet_name=0)
-a = df.to_numpy()
+df = pd.read_excel("file.xlsx", sheet_name=0) # чтение xlsx файла с матрицей
+a = df.to_numpy() # обработка матрицы как матрицы библиотеки numpy
 sns.set(font_scale=1)
-hmp = sns.heatmap(a, cmap='Blues', linewidths= .3, yticklabels=['Подписи'])
+hmp = sns.heatmap(a, cmap='Blues', linewidths= .3, yticklabels=['Подписи']) # задание необходимых настроек для построения диаграммы
 plt.title("Название", fontsize =20)
-plt.show()
+plt.show() # построение диаграммы
